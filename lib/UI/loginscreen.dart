52d4b410dc3b3f6ff64ee9cc8dca25eff845bead
Irfan Ullah Sher Alam Khan <irfan.khan@ta.gov.ae>
2023-10-04 15:53:34 +0400

    theme and login page design

--- lib/UI/loginscreen.dart
+++ lib/UI/loginscreen.dart
@@ -1,30 +1,142 @@
-import 'package:assetmanagment/widget/loginform.dart';
 import 'package:flutter/material.dart';
-class LoginSecreen extends StatelessWidget{
+import 'package:flutter_svg/flutter_svg.dart';
 
-const LoginSecreen({super.key});
+class LoginFrom extends StatefulWidget {
+  const LoginFrom({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<LoginFrom> createState() => _LoginState();
+}
+
+class _LoginState extends State<LoginFrom> {
+  final GlobalKey<FormState> _formKey = GlobalKey();
+
+  final FocusNode _focusNodePassword = FocusNode();
+  final TextEditingController _controllerUsername = TextEditingController();
+  final TextEditingController _controllerPassword = TextEditingController();
+
+  bool _obscurePassword = true;
+
   @override
   Widget build(BuildContext context) {
-    return   Scaffold(
-           body: Column(
-             children: [
-             const SizedBox(height: 100),
-               Center(
-                 child: Container(
-                   width: 100,
-                   height: 100,
-                 decoration: const BoxDecoration(
-                  image:  DecorationImage(
-                 image:  AssetImage("assets/icone/logo.png"),
-                 fit: BoxFit.fill,
-                      )
-                    )
-                 ),
-               ),
-              const LoginFrom(),
-             ],
-           ),
+    double height = MediaQuery.of(context).size.height;
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        // backgroundColor: Color.fromARGB(a, r, g, b),
+        body: SingleChildScrollView(
+          child: SizedBox(
+            height: height,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 78.9,
+                    ),
+                    Center(
+                      child: SvgPicture.asset('assets/icone/logo.svg',
+                          width: 66.4, height: 51.2),
+                    ),
+                    const SizedBox(
+                      height: 42.9,
+                    ),
+                    Text("الدخول",
+                        style: Theme.of(context).textTheme.displayLarge),
+                    const SizedBox(height: 13),
+                    Text("سعداء برجوعك الينا مجددا",
+                        style: Theme.of(context).textTheme.displaySmall),
+                    const SizedBox(height: 37),
+                    TextFormField(
+                      controller: _controllerUsername,
+                      keyboardType: TextInputType.name,
+                      decoration: const InputDecoration(
+                        labelText: "Username",
+                        prefixIcon: Icon(Icons.person_outline),
+                      ),
+                      onEditingComplete: () =>
+                          _focusNodePassword.requestFocus(),
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return "Please enter username.";
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      controller: _controllerPassword,
+                      focusNode: _focusNodePassword,
+                      obscureText: _obscurePassword,
+                      keyboardType: TextInputType.visiblePassword,
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        prefixIcon: const Icon(Icons.password_outlined),
+                        suffixIcon: IconButton(
+                            onPressed: () {
+                              setState(() {
+                                _obscurePassword = !_obscurePassword;
+                              });
+                            },
+                            icon: _obscurePassword
+                                ? const Icon(Icons.visibility_outlined)
+                                : const Icon(Icons.visibility_off_outlined)),
+                      ),
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return "Please enter password.";
+                        }
+
+                        return null;
+                      },
+                    ),
+                    const Spacer(),
+                    Align(
+                      alignment: FractionalOffset.bottomCenter,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Theme.of(context).primaryColor,
+                          fixedSize: const Size(327, 65),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(36),
+                          ),
+                        ),
+                        onPressed: () {
+                          if (_formKey.currentState?.validate() ?? false) {
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) {
+                                  return const LoginFrom();
+                                },
+                              ),
+                            );
+                          }
+                        },
+                        child: Text("Login",
+                            style: Theme.of(context).textTheme.displaySmall),
+                      ),
+                    ),
+                    const SizedBox(height: 50),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
+
+  @override
+  void dispose() {
+    _focusNodePassword.dispose();
+    _controllerUsername.dispose();
+    _controllerPassword.dispose();
+    super.dispose();
+  }
 }
-  
--- lib/UI/splashscreen.dart
+++ lib/UI/splashscreen.dart
@@ -1,53 +1,47 @@
-
 import 'dart:async';
-
 import 'package:assetmanagment/UI/loginscreen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class SplashScreen extends StatefulWidget{
-
-const  SplashScreen({super.key});
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
 
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  
-
- @override
+  @override
   void initState() {
     super.initState();
-    Timer(const Duration(seconds: 10),
-          ()=>Navigator.pushReplacement(context,MaterialPageRoute(builder:(context) => const LoginSecreen())));
+    Timer(
+        const Duration(seconds: 3),
+        () => Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => const LoginFrom())));
   }
 
   @override
   Widget build(BuildContext context) {
-    Size size =MediaQuery.sizeOf(context);
-    return  Scaffold(
-      body:Container(
-       height:size.height,
-       width:size.width,
-      decoration:const BoxDecoration(color:Color.fromRGBO(247 ,248, 249, 1)),
-        child: Column(
-          children:<Widget> [
-            Expanded(
-              child:  Center(child: Image.asset('assets/icone/logo.png'),
-              ),
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Center(
+              child: SvgPicture.asset('assets/icone/logo.svg',
+                  width: 173, height: 134),
             ),
-            const Align(
-             alignment: Alignment.bottomCenter,
-             child: Text("From"),
-        ),
-         const SizedBox(height: 60),
-           const Align(
-             alignment: Alignment.bottomCenter,
-             child: Text("Transport Authority"),
-        ),
-       const SizedBox(height: 30)
-          ],
-        ),
+          ),
+          const Align(
+            alignment: Alignment.bottomCenter,
+            child: Text("From"),
+          ),
+          const SizedBox(height: 60),
+          const Align(
+            alignment: Alignment.bottomCenter,
+            child: Text("Transport Authority"),
+          ),
+          const SizedBox(height: 30)
+        ],
       ),
     );
   }
